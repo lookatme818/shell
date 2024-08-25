c3d0f7e17e07b471bc2544fdd8da6968808a1cfe
lookatme818 <80871033+lookatme818@users.noreply.github.com>
2024-08-25 01:12:03 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -6,7 +6,7 @@ set -x
 ################
 # > Edit this file below to complete the homework assignment
 # > Add your code below each comment to complete the tasks
-
+answered
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
 mkdir dir1 dir2 dir3 dir4 dir5
 # 2. List the contents of the current working directory to verify the presence of the 5 directories
